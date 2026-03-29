f45c300aa39ead0076ee9184e1caf1be162dc562
zachdyszelwcu <143730399+zachdyszelwcu@users.noreply.github.com>
2026-03-29 15:58:07 -0400

    Update yolov8.py

--- yolov8/yolov8.py
+++ yolov8/yolov8.py
@@ -1,3 +1,4 @@
+/*
 from flask import Flask, request, send_file
 import numpy as np
 import cv2
@@ -31,3 +32,5 @@ def detect():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002)
+
+*/
