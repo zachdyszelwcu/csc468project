d0b9f2a95730317f38bf02c6fc64a46bc8b4a102
zachdyszelwcu <143730399+zachdyszelwcu@users.noreply.github.com>
2026-03-29 15:18:52 -0400

    Update yolov8.py

--- yolov8/yolov8.py
+++ yolov8/yolov8.py
@@ -3,28 +3,18 @@ import cv2
 
 model = YOLO("yolov8s.pt")
 
-def get_camera():
-    for i in range(5):
-        cap = cv2.VideoCapture(i)
-        if cap.isOpened():
-            print(f"Using camera index {i}")
-            return cap
-    print("No camera found")
-    return None
-
 def generate_frames():
-    cap = get_camera()
-
-    if cap is None:
-        return
+    cap = cv2.VideoCapture(0)
 
     while True:
         success, frame = cap.read()
         if not success:
             break
 
+        results = model(frame)
+        annotated = results[0].plot()
 
-        _, buffer = cv2.imencode('.jpg', frame)
+        _, buffer = cv2.imencode('.jpg', annotated)
         frame_bytes = buffer.tobytes()
 
         yield (b'--frame\r\n'
