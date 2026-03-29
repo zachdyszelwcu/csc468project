1321a63353f9ffde922210247d46dda6ed3aea52
zachdyszelwcu <143730399+zachdyszelwcu@users.noreply.github.com>
2026-03-29 14:44:33 -0400

    Update app.py

--- yolov8/app.py
+++ yolov8/app.py
@@ -1,41 +1,16 @@
-from flask import Flask, request, jsonify
-import base64
-import numpy as np
-import cv2
-from ultralytics import YOLO
+from flask import Flask, Response
+from yolov8 import generate_frames
 
 app = Flask(__name__)
-
-model = YOLO("yolov8s.pt")
 
 @app.route("/")
 def home():
     return "Backend running"
 
-@app.route("/detect", methods=["POST"])
-def detect():
-    data = request.json["image"]
-
-    encoded = data.split(",")[1]
-    img_bytes = base64.b64decode(encoded)
-
-    np_arr = np.frombuffer(img_bytes, np.uint8)
-    img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-
-    if img is None:
-        return jsonify({ "image": None })
-
-    img = cv2.resize(img, (640, 480))
-
-    results = model(img, conf=0.3)
-    annotated = results[0].plot()
-
-    _, buffer = cv2.imencode('.jpg', annotated)
-    img_base64 = base64.b64encode(buffer).decode('utf-8')
-
-    return jsonify({
-        "image": img_base64
-    })
+@app.route("/video")
+def video():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5002)
