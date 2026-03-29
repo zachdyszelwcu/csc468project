6f2647fda21c33b924884e85be5018f58228b9d5
zachdyszelwcu <143730399+zachdyszelwcu@users.noreply.github.com>
2026-03-29 15:56:48 -0400

    Update app.py

--- yolov8/app.py
+++ yolov8/app.py
@@ -1,16 +1,33 @@
-from flask import Flask, Response
-from yolov8 import generate_frames
+from flask import Flask, request, send_file
+import numpy as np
+import cv2
+from ultralytics import YOLO
+import io
 
 app = Flask(__name__)
+
+model = YOLO("yolov8s.pt")
 
 @app.route("/")
 def home():
     return "Backend running"
 
-@app.route("/video")
-def video():
-    return Response(generate_frames(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route("/detect", methods=["POST"])
+def detect():
+    file = request.files["image"].read()
+
+    npimg = np.frombuffer(file, np.uint8)
+    img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+
+    results = model(img)
+    annotated = results[0].plot()
+
+    _, buffer = cv2.imencode(".jpg", annotated)
+
+    return send_file(
+        io.BytesIO(buffer),
+        mimetype="image/jpeg"
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002)
