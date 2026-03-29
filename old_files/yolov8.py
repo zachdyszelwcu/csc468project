b21ebf4d2d712e103f0c9e38907563eb2abc5fb8
zachdyszelwcu <143730399+zachdyszelwcu@users.noreply.github.com>
2026-03-29 17:15:25 -0400

    Update yolov8.py

--- old_files/yolov8.py
+++ old_files/yolov8.py
@@ -1,36 +1,21 @@
-/*
-from flask import Flask, request, send_file
-import numpy as np
-import cv2
 from ultralytics import YOLO
-import io
-
-app = Flask(__name__)
+import cv2
 
 model = YOLO("yolov8s.pt")
 
-@app.route("/")
-def home():
-    return "Backend running"
+def generate_frames():
+    cap = cv2.VideoCapture(0)
 
-@app.route("/detect", methods=["POST"])
-def detect():
-    file = request.files["image"].read()
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
 
-    npimg = np.frombuffer(file, np.uint8)
-    img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+        results = model(frame)
+        annotated = results[0].plot()
 
-    results = model(img)
-    annotated = results[0].plot()
+        _, buffer = cv2.imencode('.jpg', annotated)
+        frame_bytes = buffer.tobytes()
 
-    _, buffer = cv2.imencode(".jpg", annotated)
-
-    return send_file(
-        io.BytesIO(buffer),
-        mimetype="image/jpeg"
-    )
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002)
-
-*/
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
