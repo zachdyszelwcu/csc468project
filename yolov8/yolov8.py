38391166e8ae6e8757fc543af6b634273f16aed5
zachdyszelwcu <143730399+zachdyszelwcu@users.noreply.github.com>
2026-03-29 14:36:23 -0400

    Update yolov8.py

--- yolov8/yolov8.py
+++ yolov8/yolov8.py
@@ -4,38 +4,18 @@ import cv2
 model = YOLO("yolov8s.pt")
 
 def generate_frames():
-    cap = None
+    cap = cv2.VideoCapture(0)
 
-    for i in range(5):
-        cap = cv2.VideoCapture(i, cv2.CAP_DSHOW)
-        print("Trying index", i, "Opened:", cap.isOpened())
-        if cap.isOpened():
-            print("Using camera index:", i)
+    while True:
+        success, frame = cap.read()
+        if not success:
             break
 
-    if not cap or not cap.isOpened():
-        print("ERROR: No camera found")
-        return
+        results = model(frame)
+        annotated = results[0].plot()
 
-    try:
-        while True:
-            success, frame = cap.read()
-            print("Frame read:", success)
+        _, buffer = cv2.imencode('.jpg', annotated)
+        frame_bytes = buffer.tobytes()
 
-            if not success:
-                continue
-
-            results = model(frame)
-            annotated = results[0].plot()
-
-            ret, buffer = cv2.imencode('.jpg', annotated)
-            if not ret:
-                continue
-
-            frame_bytes = buffer.tobytes()
-
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
-
-    finally:
-        cap.release()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
